1ce92da094476e192795bf77c95c52efc0ed108e
Thomas Bak <tombak98@gmail.com>
2023-01-02 13:34:30 -0500

    testing to make sure regular react works

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
 
 use npm run dev to start the server
+
+Next step I need to do is dynamic routes
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,12 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Layout from '../../components/layout';
+import React from 'react';
 
 export default function FirstPost() {
+
+  const [counter, setCounter] = React.useState(0)
+
   return (
     <Layout>
       <Head>
@@ -12,6 +16,8 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+      <button onClick={()=>setCounter(counter+1)}>Click Me</button>
+      <h3>{counter}</h3>
     </Layout>
   );
 }
